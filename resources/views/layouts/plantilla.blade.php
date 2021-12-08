ba6561fe737e10bfd05c9ad2e1175490a4d193df
kevinlozada1102 <kevinlozada1102@gmail.com>
2021-12-08 13:21:38 -0500

    agregar favicon

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/estilos.css')}}" rel="stylesheet" >
     <link href="{{ asset('css/grid1.css')}}" rel="stylesheet" >
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico')}}">
 
     <title>DATOS PERSONALES</title>
 </head>
